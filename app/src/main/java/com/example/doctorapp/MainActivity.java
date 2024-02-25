a148c50b7d466a43cbeb1d261179c7dd0c440c4b
Angelo275 <palangel275@gmail.com>
2024-02-25 01:56:00 -0800

    made few changes but it is not working properly

--- app/src/main/java/com/example/doctorapp/MainActivity.java
+++ app/src/main/java/com/example/doctorapp/MainActivity.java
@@ -6,23 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-Button getstarted;
+    Button getStarted;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getstarted = findViewById(R.id.getstarted);
-
-        getstarted.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,DashboardActivity.class));
-            }
+        getStarted = findViewById(R.id.getStart);
+        getStarted.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
+            startActivity(intent);
         });
-
     }
 }
